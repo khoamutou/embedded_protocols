b1d7e164c7fe0dda819baed2487037377669c01b
khoamutou <131346173+khoamutou@users.noreply.github.com>
2023-12-10 22:56:30 +0700

    Delete UART_PC directory

--- UART_PC/Objects/stm32f10x_crc.d
+++
@@ -1,6 +0,0 @@
-.\objects\stm32f10x_crc.o: StdPeriph_Driver\src\stm32f10x_crc.c
-.\objects\stm32f10x_crc.o: .\StdPeriph_Driver\inc\stm32f10x_crc.h
-.\objects\stm32f10x_crc.o: .\CMSIS\stm32f10x.h
-.\objects\stm32f10x_crc.o: .\CMSIS\core_cm3.h
-.\objects\stm32f10x_crc.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\stm32f10x_crc.o: .\CMSIS\system_stm32f10x.h
--- UART_PC/Objects/stm32f10x_pwr.d
+++
@@ -1,7 +0,0 @@
-.\objects\stm32f10x_pwr.o: StdPeriph_Driver\src\stm32f10x_pwr.c
-.\objects\stm32f10x_pwr.o: .\StdPeriph_Driver\inc\stm32f10x_pwr.h
-.\objects\stm32f10x_pwr.o: .\CMSIS\stm32f10x.h
-.\objects\stm32f10x_pwr.o: .\CMSIS\core_cm3.h
-.\objects\stm32f10x_pwr.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\stm32f10x_pwr.o: .\CMSIS\system_stm32f10x.h
-.\objects\stm32f10x_pwr.o: .\StdPeriph_Driver\inc\stm32f10x_rcc.h
